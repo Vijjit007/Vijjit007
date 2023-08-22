8579570b65ff0f72710baff1f41e3cf0246fc268
vijjit100 <vijjit.srivastava100@gmail.com>
2023-08-22 15:12:57 +0530

    first commit

---
+++ Index.js
@@ -0,0 +1,32 @@
+//var x=10;
+
+/* let i=1;
+while(i<=10)
+{
+    if(i%2==0){
+        console.log(i);
+    }
+    i=i++;
+}
+
+
+for(let x=0;x<=10;x++){
+    console.log(x)
+};
+
+
+console.log('Hello OG');
+*/
+let a =5;
+let b =10;
+let c =a+b;
+console.log(c);
+
+
+let y=55;
+let z=50;
+let gwen=y*z;
+console.log(gwen);
+console.log(y);
+console.log(z);
+
